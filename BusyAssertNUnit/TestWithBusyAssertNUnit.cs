c125c5c2ef83feee3a54a7b2c0950c583576f53e
Dror Helper <dror.helper@gmail.com>
2016-06-06 11:56:10 +0300

    Added NUnit Assert with delay example

---
+++ BusyAssertNUnit/TestWithBusyAssertNUnit.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BusyAssertNUnit
+{
+    [TestFixture]
+    public class TestWithBusyAssertNUnit
+    {
+        [Test]
+        public void DifficultCalculationTest()
+        {
+            var cut = new ClassWithAsyncOperation();
+
+            cut.RunAsync(2, 3);
+
+            Assert.That(() => cut.Result, Is.EqualTo(5).After(10000, 10));
+        }
+    }
+
+    public class ClassWithAsyncOperation
+    {
+        public void RunAsync(int a, int b)
+        {
+            Task.Run(() =>
+            {
+                var sum = a + b;
+
+                Result = sum;
+            });
+        }
+
+        public int Result { get; private set; }
+    }
+}
