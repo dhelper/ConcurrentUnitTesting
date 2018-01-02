007052a7728259ac710debdc5676f7eb85bd110a
Dror Helper <dror.helper@gmail.com>
2018-01-02 16:00:14 +0200

    Renamed new tests

--- AwaitInTest/AwaitingTests.cs
+++ AwaitInTest/AwaitingTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace AwaitInTest
 {
     [TestClass]
-    public class UnitTest1
+    public class AwaitingTests
     {
         [TestMethod]
         public async void ThisTestWouldNotFound()
