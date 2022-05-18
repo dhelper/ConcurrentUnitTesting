a45718257fba06e19fb83e3dd6792d19e6fce194
Dror Helper <dhelper@amazon.com>
2022-05-18 15:04:47 +0100

    Added new test with wrapped timer

---
+++ FakeWithTimerWrapper/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FakeWithTimerWrapper")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FakeWithTimerWrapper")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("CDCD8411-D896-428E-9330-3AB6F820FEE8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FakeWithTimerWrapper/WrappingTimerTest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Timers;
+using FakeItEasy;
+using NUnit.Framework;
+
+namespace FakeWithTimerWrapper
+{
+    [TestFixture]
+    public class WrappingTimerTest
+    {
+        [Test]
+        public void ThisIsAGoodTest()
+        {
+            var fakeTimer = A.Fake<ITimer>();
+
+            var cut = new ClassWithTimer(fakeTimer);
+
+            fakeTimer.Elapsed += Raise.WithEmpty();
+
+            Assert.IsTrue(cut.SomethingImportantHappened);
+        }
+    }
+
+    public interface ITimer
+    { 
+        event EventHandler Elapsed;
+        void Start();
+        void Stop();
+    }
+    
+    public class WrappedTimer : ITimer
+    {
+        private readonly Timer _timer;
+
+        public event EventHandler Elapsed;
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+
+        public WrappedTimer(Timer timer)
+        {
+            _timer = timer;
+            _timer.AutoReset = false;
+            _timer.Elapsed += OnTimerElapsed;
+        }
+        
+        
+
+        private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                Elapsed?.Invoke(sender, e);
+            }
+            finally
+            {
+                _timer.Start();
+            }
+        }
+    }
+    public class ClassWithTimer
+    {
+        private readonly ITimer _timer;
+
+        public ClassWithTimer(ITimer timer)
+        {
+            _timer = timer;
+            _timer.Elapsed += OnTimerElapsed;
+            _timer.Start();
+        }
+
+        private void OnTimerElapsed(object sender, EventArgs e)
+        {
+            SomethingImportantHappened = true;
+        }
+
+        public bool SomethingImportantHappened { get; private set; }
+    }
+
+}
